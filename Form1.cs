aa657f1ce70d776ebdd758fd56b7564e4cdd4f0c
Bartosz Potyrała <bartosz.potyrala@microsoft.wsei.edu.pl>
2024-04-14 12:45:33 +0200

    e

--- Form1.cs
+++ Form1.cs
@@ -12,8 +12,7 @@ namespace po_cw3
 {
     public partial class Form1 : Form
     {
-        List<int> values = new List<int>();
-        List<string> operators = new List<string>();
+        List<string> values = new List<string>();
 
 
         public Form1()
@@ -23,86 +22,86 @@ namespace po_cw3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 1;
-            values.Add(1);
+            textBoxOutput.Text = textBoxOutput.Text + "1";
+            values.Add("1");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 2;
-            values.Add(2);
+            textBoxOutput.Text = textBoxOutput.Text + "2";
+            values.Add("2");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 3;
-            values.Add(3);
+            textBoxOutput.Text = textBoxOutput.Text + "3";
+            values.Add("3");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 4;
-            values.Add(4);
+            textBoxOutput.Text = textBoxOutput.Text + "4";
+            values.Add("4");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 5;
-            values.Add(5);
+            textBoxOutput.Text = textBoxOutput.Text + "5";
+            values.Add("5");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 6;
-            values.Add(6);
+            textBoxOutput.Text = textBoxOutput.Text + "6";
+            values.Add("6");
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 7;
-            values.Add(7);
+            textBoxOutput.Text = textBoxOutput.Text + "7";
+            values.Add("7");
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 8;
-            values.Add(8);
+            textBoxOutput.Text = textBoxOutput.Text + "8";
+            values.Add("8");
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 9;
-            values.Add(9);
+            textBoxOutput.Text = textBoxOutput.Text + "9";
+            values.Add("9");
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = textBoxOutput.Text + 0;
-            values.Add(0);
+            textBoxOutput.Text = textBoxOutput.Text + "0";
+            values.Add("0");
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
             textBoxOutput.Text = textBoxOutput.Text + "+";
-            operators.Add(textBoxOutput.Text);
+            values.Add("+");
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
             textBoxOutput.Text = textBoxOutput.Text + "-";
-            operators.Add(textBoxOutput.Text);
+            values.Add("-");
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
             textBoxOutput.Text = textBoxOutput.Text + "*";
-            operators.Add(textBoxOutput.Text);
+            values.Add("*");
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
             textBoxOutput.Text = textBoxOutput.Text + "/";
-            operators.Add(textBoxOutput.Text);
+            values.Add("/");
         }
 
         private void button15_Click(object sender, EventArgs e)
